c28032df3b717af3f73bc5810035d85ac861ed5b
Junip Dewan <52928789+junip-dewan@users.noreply.github.com>
2020-06-05 18:42:18 +0530

    added dependencies

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './scss/main.scss'
+const App = () => {
+  return (
+    <div>asd</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
