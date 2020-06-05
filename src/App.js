1669eacaa802785d2773914fdf13b87d4cbcbccf
Junip Dewan <junipd2@gmail.com>
2020-06-06 00:11:39 +0530

    Integrated the apollo client with the graphql schema and apis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import './scss/main.scss'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ShowCountries from '../src/components/ShowCountries'
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'https://countries-274616.ew.r.appspot.com/'
+})
+
 const App = () => {
   return (
-    <div>asd</div>
+   <ApolloProvider client={client}>
+     <div className=""> 
+        <ShowCountries/>
+     </div>
+   </ApolloProvider>
   )
 }
 
